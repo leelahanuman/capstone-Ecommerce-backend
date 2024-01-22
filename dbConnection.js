38dff235493a9f97a9fce1bdff74a7f378fcbd48
Leela Hanuman Madasu <101544693+leelahanuman@users.noreply.github.com>
2024-01-22 16:44:21 +0530

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    // console.log(process.env.CONNECTION_STRING);
+    console.log(process.env.CONNECTION_STRING,'okay');
     await mongoose.connect("mongodb+srv://madasuleelahanuman:FvWFTGyEu4nt8pCj@cluster0.aroi7ko.mongodb.net/fomo-db");
     console.log("DB CONNECTED SUCCESFULLY");
   } catch (error) {
