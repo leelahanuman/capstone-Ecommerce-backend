430f70b7edc52f55ab21dc1a42cd91bbb44eeffd
Leela Hanuman Madasu <101544693+leelahanuman@users.noreply.github.com>
2026-01-19 04:54:45 -1200

    Enhance CORS settings in index.js
    Updated CORS configuration to allow all origins and specified methods.

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const app=express();
 const connection=require("./dbConnection");
 connection();
 
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+
 app.use("/auth",require("./Routes/authentication"))
 app.use("/products",require("./Routes/products"))
 app.use("/cart",require("./Routes/addtocart"))
@@ -16,3 +22,4 @@ app.use('*',(req,res)=>res.status(404).json({message:'No such end points exists'
 app.listen(process.env.PORT,()=>{
     console.log("Listening on port",process.env.PORT);
 })
+
