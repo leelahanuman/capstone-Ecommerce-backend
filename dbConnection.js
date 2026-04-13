e3cd1500fbc2251fb26206ed8ef10da7bd812b8a
Leela Hanuman Madasu <101544693+leelahanuman@users.noreply.github.com>
2026-04-14 00:04:19 +0530

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.CONNECTION_STRING);
     console.log("MongoDB Connected");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
