7d74e8788b972d4cde5031475a6f8255a768b145
Leela Hanuman Madasu <101544693+leelahanuman@users.noreply.github.com>
2026-05-13 21:59:26 +0530

    Refactor MongoDB connection to connectDB function
    Refactor MongoDB connection function to handle errors and exit process on failure.

--- dbConnection.js
+++ dbConnection.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
-const connect = async () => {
+const connectDB = async () => {
   try {
-     await mongoose.connect(process.env.CONNECTION_STRING);
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.error("MongoDB connection failed:", err.message);
+    await mongoose.connect(process.env.CONNECTION_STRING);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1);
   }
 };
 
-module.exports = connect;
+module.exports = connectDB;
